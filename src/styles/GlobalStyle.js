bc3fe5f919dfaa88b7bb06dd45310d16e5daa953
da-yooom <kdh941110@gmail.com>
2022-03-27 21:39:01 +0900

    💄 GlobalStyle 컴포넌트 생성 및 적용

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+  body {
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+      'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button,
+  input,
+  select,
+  textarea {
+    background-color: transparent;
+    border: 0;
+
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  button {
+    padding: 0;
+  }
+
+  ul,
+  ol {
+    padding-left: 0;
+    list-style: none;
+  }
+
+  address {
+    font-style: normal;
+  }
+`;
+
+export default GlobalStyle;
