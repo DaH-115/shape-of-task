3dac3397029f11465adc5bfee733509f51222743
dahyeon <kdh941110@gmail.com>
2023-09-02 04:56:29 +0900

    ⬆️ @types/lodash 설치 및 적용

--- src/hooks/useGetwindowWidth.tsx
+++ src/hooks/useGetwindowWidth.tsx
@@ -2,22 +2,22 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { debounce } from 'lodash';
 
-// ✔️ windowWidth가 desktopSize이상이면 메인 화면으로 navigate합니다.
+// ✔️ windowWidth가 desktopSize이상이면 메인 화면으로 navigate합니다
 const useGetwindowWidth = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const navigate = useNavigate();
-  const desktopSize = 1024;
+  const tabletSize = 768;
 
   useEffect(() => {
     const resizeHandler = () => setWindowWidth(window.innerWidth);
     window.addEventListener('resize', debounce(resizeHandler, 200));
 
-    windowWidth >= desktopSize && navigate('/');
+    windowWidth >= tabletSize && navigate('/');
 
     return () => window.removeEventListener('resize', resizeHandler);
   }, [windowWidth, navigate]);
 
-  return { windowWidth, desktopSize };
+  return { windowWidth, tabletSize };
 };
 
 export default useGetwindowWidth;
