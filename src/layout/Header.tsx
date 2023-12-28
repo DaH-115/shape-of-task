77dfa18934c0272f82d283f40925c73fa21af231
DaHyeon <kdh941110@gmail.com>
2023-12-29 03:33:41 +0900

    🚧 Header 컴포넌트 교체 & AddBtn 디렉토리 위치 수정

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,109 +1,64 @@
-import React, { useState, useCallback } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
 import styled from 'styled-components';
-import useGetWindowWidth from 'hooks/useGetWindowWidth';
+import { IoMenu } from 'react-icons/io5';
 import { ReactComponent as Logo } from 'assets/Logo.svg';
-import SlideMenu from 'components/SlideMenu';
 
-const Header = () => {
-  const [isOpen, setIsOpen] = useState<string>('false');
-  const [isSelect, setIsSelect] = useState<boolean>(false);
-  const location = useLocation();
-  const tabletSize = 768;
-  const { windowWidth } = useGetWindowWidth(tabletSize);
+const NewHeader = () => {
+  const [isToggle, setIsToggle] = React.useState(false);
+  const todayDate = React.useMemo(() => new Date(), []);
+  const today = todayDate.toLocaleDateString();
 
-  const slideMenuOpenHandler = useCallback(() => {
-    if (isOpen === 'true') {
-      setIsOpen('false');
-      document.body.style.overflow = 'auto';
-    } else {
-      setIsOpen('true');
-      document.body.style.overflow = 'hidden';
-    }
-
-    setIsSelect((prev) => !prev);
-  }, [isOpen]);
+  const menuToggleHandler = React.useCallback(() => {
+    setIsToggle((prev) => !prev);
+    console.log(isToggle, '슬라이드 메뉴 오픈');
+  }, [isToggle]);
 
   return (
-    <HeaderWrapper>
-      <StyledLogo />
-      <MeueWrapper>
-        <Link to='/'>
-          <MenuBtn $isselected={location.pathname === '/' ? 'true' : 'false'}>
-            {'할 일'}
-          </MenuBtn>
-        </Link>
-        {windowWidth <= tabletSize && (
-          <Link to='/figure-list'>
-            <MenuBtn
-              $isselected={
-                location.pathname === '/figure-list' ? 'true' : 'false'
-              }
-            >
-              {'완료된 일'}
-            </MenuBtn>
-          </Link>
-        )}
-        <MenuBtn
-          $isselected={isSelect.toString()}
-          onClick={slideMenuOpenHandler}
-        >
-          {'설정'}
-        </MenuBtn>
-      </MeueWrapper>
-      <SlideMenu isopen={isOpen} slideMenuHandler={slideMenuOpenHandler} />
-    </HeaderWrapper>
+    <>
+      <HeaderWrapper>
+        <Wrapper>
+          <LogoWrapper>
+            <StyledLogo />
+          </LogoWrapper>
+          {/* TODO: 슬라이드 메뉴 */}
+          <IoMenu fontSize={'2rem'} onClick={menuToggleHandler} />
+        </Wrapper>
+        <Contents>
+          {/* TODO: 날씨 API 적용 */}
+          {today}
+          {' ☀️ 맑음'}
+        </Contents>
+      </HeaderWrapper>
+    </>
   );
 };
 
-export default React.memo(Header);
+export default React.memo(NewHeader);
 
-const HeaderWrapper = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
+const HeaderWrapper = styled.div`
   width: 100%;
-  padding: 1rem;
-  padding-top: 1.4rem;
+  color: ${({ theme }) => theme.commonColors.black};
   background-color: #fff;
+  padding: 2rem 1rem 1rem;
+`;
 
-  border-bottom: 0.1rem solid ${({ theme }) => theme.commonColors.light_gray};
+const LogoWrapper = styled.div`
+  width: 30%;
 `;
 
 const StyledLogo = styled(Logo)`
-  width: auto;
-  height: 1.2rem;
-
-  ${({ theme }) => theme.device.tablet} {
-    height: 1rem;
-  }
+  width: 100%;
+  height: 100%;
 `;
 
-const MeueWrapper = styled.div`
+const Contents = styled.div`
+  margin-top: 0.2rem;
+`;
+
+const Wrapper = styled.div`
   display: flex;
-  color: ${({ theme }) => theme.commonColors.gray};
-`;
-
-const MenuBtn = styled.div<{ $isselected: string }>`
-  font-size: 1.2rem;
-  font-weight: ${({ $isselected }) => ($isselected === 'true' ? '700' : '500')};
-
-  color: ${({ theme, $isselected }) =>
-    $isselected === 'true'
-      ? theme.commonColors.black
-      : theme.commonColors.gray};
-
-  padding: 0.2rem 0.4rem;
-  cursor: pointer;
-
-  &:hover,
-  &:active {
-    color: ${({ theme }) => theme.commonColors.black};
-    transition: color 0.2s ease-in-out;
-  }
-
-  ${({ theme }) => theme.device.tablet} {
-    font-size: 1rem;
-  }
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
 `;
